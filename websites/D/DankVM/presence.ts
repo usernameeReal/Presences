28cd0b142d7d3d2203787aa28e6b535f34c3bd8b
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-08-12 12:13:57 +0200

    Format document

--- websites/D/DankVM/presence.ts
+++ websites/D/DankVM/presence.ts
@@ -1,17 +1,17 @@
 const presence = new Presence({
-    clientId: "1007587237355065364"
-    }),
-    strings = presence.getStrings({
-      play: "presence.playback.playing",
-      pause: "presence.playback.paused"
-    });
-    presence.on("UpdateData", async () => {
-        const presenceData: PresenceData = {
-          largeImageKey: "dank",
+    clientId: "1007587237355065364",
+  }),
+  strings = presence.getStrings({
+    play: "presence.playback.playing",
+    pause: "presence.playback.paused",
+  });
+presence.on("UpdateData", async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: "dank",
 
-          details: window.vmName ? `In a VM: ${window.vmName}` : "Picking a VM",
-          state: "dank-vm.xyz",
-        };
-        if (presenceData.details) presence.setActivity(presenceData);
-        else presence.setActivity();
-      });
+    details: window.vmName ? `In a VM: ${window.vmName}` : "Picking a VM",
+    state: "dank-vm.xyz",
+  };
+  if (presenceData.details) presence.setActivity(presenceData);
+  else presence.setActivity();
+});
